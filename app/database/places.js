05bb2b97827132ddb7294a7897b8c6e570ea5602
Mochamad Gufron <mgufronefendi@gmail.com>
2017-01-19 14:16:42 +0700

    add more methods

--- app/database/places.js
+++ app/database/places.js
@@ -1,9 +1,34 @@
 import database from './database';
 
-export const writeNewPlace = (data) => {
-  const newKey = database.ref().child('places').push().key;
+const resource = 'places';
+export const addNewPlace = (name, location = {}) => {
+  const newKey = database.ref().child(resource).push().key;
   const newPlace = {};
-  newPlace[`/places/${newKey}`] = data;
+  newPlace[`/${resource}/${newKey}`] = {
+    name,
+    location,
+  };
   return database.ref().update(newPlace);
 };
-export const readPlaces = () => database.ref('/places').once('value');
+export const updatePlace = (key, name, location = {}) => {
+  const update = {};
+  update[`${resource}/${key}`] = { name, location };
+  return database.ref().update();
+};
+export const deletePlace = (key) => {
+  const currentPlace = {};
+  currentPlace[`${resource}/${key}`] = null;
+  return database.ref().update(deletePlace);
+};
+export const getPlaces = () => database.ref('/places').once('');
+
+export const addMenuToPlace = (keyPlace, name, price, isFavorite = false) => {
+  const newKey = database.ref().child(`/${resource}/${keyPlace}/menus/`).push().key;
+  const newPlace = {};
+  newPlace[`/${resource}/${keyPlace}/menus/${newKey}`] = {
+    name,
+    price,
+    is_favorite: isFavorite,
+  };
+  return database.ref().update(newPlace);
+};
